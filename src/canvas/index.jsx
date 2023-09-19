13ad5a8a79d53329939904a0998b98c604b9743e
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2023-09-19 22:34:07 +0530

    backside

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -3,13 +3,24 @@ import { Canvas } from '@react-three/fiber'
 import { Environment, Center } from '@react-three/drei';
 import TShirt from './TShirt';
 import Camera from './Camera';
+import { BackSide } from 'three';
 
 const CanvasIndex = () => {
   return (
-  <Canvas>
+  <Canvas
+  shadows
+  camera={{
+    position:[0,0,0],
+    fov:25
+
+  }}
+  gl={{preserveDrawingBuffer:true}}
+  className='w-full max-w-full h-full transition-all ease-in'
+  >
     <ambientLight intensity={0.5}/>
     <Environment preset='city' />
     <Camera>
+      <BackSide/>
     <Center>
       <TShirt/>
     </Center>
